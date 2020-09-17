f27bbd681265a93322cb40e4bcf0f4e3bd140222
Jérémy SIMONKLEIN <jeremy.simonklein@gmail.com>
2020-09-17 16:08:54 +0200

    Added cookie design

---
+++ src/CookiePart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CookiePart = () => {
+    return <div>
+        <p>Nombre de cookies: 0</p>
+        <p>Cookies par seconde: 0</p>
+        <img src="https://image.flaticon.com/icons/png/512/614/614131.png" />
+    </div>
+}
+
+export default CookiePart

1068dd8c8e5f0b129805ea6b3de2ea68c163f7f0
Jérémy SIMONKLEIN <jeremy.simonklein@gmail.com>
2020-09-17 16:32:02 +0200

    Added addCookie method in CookiePart component

--- src/CookiePart.js
+++ src/CookiePart.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const CookiePart = () => {
+    const [cookie_counter, setCookieCounter] = useState(10)
+
+    const addCookie = () => {
+        setCookieCounter(cookie_counter + 1)
+    }
+
     return <div>
-        <p>Nombre de cookies: 0</p>
+        <p>Nombre de cookies : {cookie_counter}</p>
         <p>Cookies par seconde: 0</p>
-        <img src="https://image.flaticon.com/icons/png/512/614/614131.png" />
+        <img 
+            src="https://image.flaticon.com/icons/png/512/614/614131.png"
+            onClick={addCookie}
+        />
     </div>
 }
 
